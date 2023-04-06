2573d9d28dd2c88cdf9e18b6609dfe034321e4a4
Ana Matei <anamatei.099@gmail.com>
2023-04-06 17:00:15 +0200

    Added rtk api call

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable quotes */
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
   const genreTitle = "Pop";
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
 
   return (
     <div className="flex flex-col">
@@ -21,7 +27,7 @@ const Discover = () => {
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, index) => (
+        {data?.map((song, index) => (
           <SongCard key={song.key} song={song} index={index} />
         ))}
       </div>
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,23 @@
+/* eslint-disable quotes */
+// API CALLS
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        // eslint-disable-next-line comma-dangle
+        "e330df66f0mshddf01a984dc2daap10e4b5jsned5d7bd4ad7c"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
